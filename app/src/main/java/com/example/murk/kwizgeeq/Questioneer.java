647b1ded1f509b6468e2d224d89ba58550617bf0
Marcus Lindvärn <murk@live.se>
2017-03-30 13:51:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/murk/kwizgeeq/Questioneer.java
@@ -0,0 +1,13 @@
+package com.example.murk.kwizgeeq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Questioneer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questioneer);
+    }
+}
